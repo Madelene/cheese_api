71644b250b1621b9466f3b88902d5f6b5991f013
Madelene Campos <madelenecampos@gmail.com>
2016-03-24 15:55:58 -0400

    Added validations in the user model and logic in the controller in order to render the correct json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-	
+	protect_from_forgery
+
+	expose(:username)					{ params[:username] }
+	expose(:password)					{ params[:password] }
+	expose(:current_user)			{ User.find_by username: username }
+
+	def get
+		user = User.find(current_user: current_user).as_json
+	end
+
+	def login
+		user = User.find_by(username: username)
+
+		if user 
+			if user.password == password
+				render json: { users: current_user }, status: 200
+				else
+				render json: { message: "Your password is incorrect. Please try again" }, status: 200
+			end
+		else
+			render json: { message: "User does not exist" }, status: 200
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true
+	validates :password, presence: true, 
+						 uniqueness: true,
+						 confirmation: true,
+             			 length: {within: 1..8}
+
+	user = User.new
+	
 end
