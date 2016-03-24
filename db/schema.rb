d825d191e1e55d57b1a60cdd18268c984eba9457
Madelene Campos <madelenecampos@gmail.com>
2016-03-24 15:58:31 -0400

    Created a Cheese model and migrated the db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324193521) do
+ActiveRecord::Schema.define(version: 20160324195729) do
+
+  create_table "cheeses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
