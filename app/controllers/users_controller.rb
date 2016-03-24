7151322869d510055f27180add7e848a7fb71689
Madelene Campos <madelenecampos@gmail.com>
2016-03-24 16:20:29 -0400

    Wrote get method in Users controller to have the json display correctly when a user is called by their id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
 	protect_from_forgery
 
+	expose(:id)								{ params[:id] }
 	expose(:username)					{ params[:username] }
 	expose(:password)					{ params[:password] }
 	expose(:current_user)			{ User.find_by username: username }
 
 	def get
-		user = User.find(current_user: current_user).as_json
+		user = User.find_by(id: id)
+
+		if user
+			render json: { users: user }, status: 200
+		else
+			render json: { message: "There is no user to be found" }, status: 200
+		end
 	end
 
 	def login
