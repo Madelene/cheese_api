9414db671c87efa6e34434a7ced594a4c672b1d9
Madelene Campos <madelenecampos@gmail.com>
2016-03-24 15:40:12 -0400

    Generated a Users controller and created two endpoints in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/user/:id'        => 'users#get'
+  post '/login'          => 'users#login'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
