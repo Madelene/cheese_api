f40a7f055d1a3780f3198e608d7daf14a992852f
Madelene Campos <madelenecampos@gmail.com>
2016-03-24 18:11:52 -0400

    Added private method in Cheeses Controller to use Rails 4 strong parameters so we won't need attr_accessible in the model

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -15,16 +15,21 @@ class CheesesController < ApplicationController
 
 	end
 
-
-
 	def get_by_user
-		cheese = Cheese.find(id: id)
+		cheese = []
+		Cheese.
+		# Cheese.find(id: id)
 
 		if user.cheese
 			render json: { cheeses: name }, status: 200
 		else
 			render json: { message: "This user apparently does not like cheese" }, status: 200
 		end
+	end
 
+	private
+
+	def cheese_params
+		params.require(:cheese).permit(:name)
 	end
 end
